f0494f7df5f8632ebb03e7dff010e95466a62e55
mkjuice <masonk@udel.edu>
2023-03-11 19:29:37 -0500

    ladst test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -207,7 +207,7 @@ export default function Navbar() {
         if(website){
           setShowLogo(false)
           document.getElementById('chain_frame').srcdoc = ""
-          document.getElementById('chain_frame').srcdoc = "<div>hello</div>";
+          document.getElementById('chain_frame').srcdoc = website.finalData;
           setfinalData(website.finalData)
           handleURL(toSearch);
           return
